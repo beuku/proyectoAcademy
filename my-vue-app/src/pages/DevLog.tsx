e7508b9c036a8409970c910b62818598a878d635
JostynBateojaOrtiz <jostynortiz87@gmail.com>
2024-12-27 15:51:25 -0300

    footer(iconos)

--- my-vue-app/src/pages/DevLog.tsx
+++ my-vue-app/src/pages/DevLog.tsx
@@ -1,3 +1,4 @@
+import Footercomponents from "../components/Footercomponents"
 import HeaderComponemts from "../components/HeaderComponemts"
 
 function DevLog() {
@@ -5,6 +6,7 @@ function DevLog() {
       <>
         <HeaderComponemts />
         <div>PagesInicial</div>
+        <Footercomponents /> 
       </>
     )
   }
