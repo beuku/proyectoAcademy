fc786b2dc534ad088781a6a71b47ce9b61fdf453
dawe-vk <nicolassanhhh@gmail.com>
2025-01-02 17:33:56 -0600

    XDDDDDD
    XDDDDDD

--- my-vue-app/src/components/HeaderComponemts.tsx
+++ my-vue-app/src/components/HeaderComponemts.tsx
@@ -35,36 +35,8 @@ function HeaderComponemts() {
             </div>
             <RegitrarComponents />
         </nav>
-        <header className="header">
+        <header className="header"><h1 className="title">Bienvenido</h1>
             <img className="baner" src="./src/assets/Cap.png" alt="banner" />
-            <h1 className="title">Bienvenido a ShaqVeyter</h1>
-        <p>
-            <h4> 
-            albion online es un mmorpg no lineal en el 
-            que escribes tu propia historia sin limitarte a
-            seguir un camino prefijado, explora un amplio
-            mundo abierto con cinco biomas unicos, todo cuant
-            o hagas tendra su repercusíon en el mundo, con su 
-            economia orientada al jugador de albion los jugadores
-            crean practicamente todo el equipo a partir de los
-            recursos que consiguen, el equipo que llevas define 
-            quien eres, cambia de arma y armadura para pasar de
-            caballero a mago o juego como una mezcla de ambas 
-            clases, aventurate en el mundo abierto y haz frente
-            a los habitantes y las criaturas de albion, inicia
-            expediciones o adentrate en mazmorras en las que
-            encontraras enemigos aun mas dificiles, enfrentate
-            a otros jugadores en encuentros en el mundo abierto, 
-            lucha por los territorios o por ciudades enteras en 
-            batallas tacticas, relajate en tu isla privada donde 
-            podras construir un hogar, cultivar cosechas, criar 
-            animales, unete a un gremio, todo es mejor cuando se 
-            trabaja en grupo  adentrate 
-            ya en el mundo de albion y escribe tu propia historia.
-
-            </h4>
-        </p>
-
         </header>
     </>
   )
--- my-vue-app/src/pages/SobreEljuego.tsx
+++ my-vue-app/src/pages/SobreEljuego.tsx
@@ -1,11 +1,12 @@
 import Footercomponents from "../components/Footercomponents"
 import HeaderComponemts from "../components/HeaderComponemts"
+import Sobrejuego from "../components/Sobrejuego"
 
 function SobreEljuego() {
     return (
       <>
         <HeaderComponemts />
-        <div>PagesInicial</div>
+        <Sobrejuego />
         <Footercomponents /> 
       </>
     )
