5f1a1498c8d0c3ff4fab80e8b098cf51ee2ff603
dawe-vk <nicolassanhhh@gmail.com>
2025-01-03 14:33:32 -0600

    imagenes y texto "estructurado"
    aaa

--- my-vue-app/src/pages/SobreEljuego.tsx
+++ my-vue-app/src/pages/SobreEljuego.tsx
@@ -1,11 +1,10 @@
 import Footercomponents from "../components/Footercomponents"
-import HeaderComponemts from "../components/HeaderComponemts"
 import Sobrejuego from "../components/Sobrejuego"
 
 function SobreEljuego() {
     return (
       <>
-        <HeaderComponemts />
+
         <Sobrejuego />
         <Footercomponents /> 
       </>
