c099e2817896c241360c6f9c6fafd61a0addf45e
dawe-vk <nicolassanhhh@gmail.com>
2025-01-07 15:23:01 -0600

    EXPLOTAAAAAAAAA
    alo

--- my-vue-app/src/pages/Descarga.tsx
+++ my-vue-app/src/pages/Descarga.tsx
@@ -1,14 +1,11 @@
-import BienvenidoComponents from "../components/BienvenidoComponents"
 import Footercomponents from "../components/Footercomponents"
 import Descargacomponents from "../components/Descargacomponents"
-import HeaderComponemts from "../components/HeaderComponemts"
+
 
 function Descarga() {
     return (
       <>
 
-        <HeaderComponemts />
-        <BienvenidoComponents />
         <Descargacomponents />
         <Footercomponents /> 
       </>
