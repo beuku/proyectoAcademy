4688e5c0ad82a042ae759524f9a4d2da4b861d0a
AlvaritoB3 <elspyroconpollo@gmail.com>
2025-01-08 10:52:40 -0300

    comunida :D

---
+++ my-vue-app/src/components/Comunidadcomponents.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import "../styless/Comunidad.css"
+
+function Comunidadcomponents() {
+    return (
+        <header>    
+        <div className="nav">
+          <img src="src/assets/icono10.png" alt="" width="80px"></img>
+          <li className="li">
+              <Link className="link" to="/">Inicio</Link>
+          </li>
+    
+          <li className="li">
+              <Link className="link" to="/SobreEljuego">Sobre el juego</Link>
+          </li>
+    
+          <li className="li">
+              <Link className="link" to="/DevLog">DevLog</Link>
+          </li>
+    
+          
+          <li className="li">
+              <Link className="link" to="/Comunidad">Comunidad</Link>
+          </li>
+    
+          <li className="li">
+              <Link className="link" to="/Descarga">Descarga</Link>     
+          </li>
+    
+          <li className="li">
+              <Link className="link" to="/AvisoLegal">Aviso legal</Link>
+          </li>
+      </div>
+    
+            <body>
+                <h1>wa</h1>
+            </body>
+        </header>
+  )
+}
+  export default Comunidadcomponents;
--- my-vue-app/src/pages/Comunidad.tsx
+++ my-vue-app/src/pages/Comunidad.tsx
@@ -1,16 +1,13 @@
-import BienvenidoComponents from "../components/BienvenidoComponents"
-import Footercomponents from "../components/Footercomponents"
-import HeaderComponemts from "../components/HeaderComponemts"
+import Footercomponents from "../components/Footercomponents";
+import Comunidadcomponents from "../components/Comunidadcomponents";
 
 function Comunidad() {
     return (
-      <>
-        <HeaderComponemts />
-        <BienvenidoComponents />
-        <div>PagesInicial</div>
-        <Footercomponents /> 
-      </>
-    )
-  }
-  
-  export default Comunidad
+        <>
+            <Comunidadcomponents />
+            <Footercomponents />
+        </>
+    );
+}
+
+export default Comunidad;
