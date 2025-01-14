0c45179fd735113bb0951ebebf8cfeb7f489d76d
dawe-vk <nicolassanhhh@gmail.com>
2025-01-14 17:03:16 -0600

    CODIGOORDENAO🗣🗣
    AAA

--- my-vue-app/src/components/AvisoLegalComponents.tsx
+++ my-vue-app/src/components/AvisoLegalComponents.tsx
@@ -1,61 +1,65 @@
-
-import "../styless/AvisoLegalStyles.css"
+import "../styless/AvisoLegalStyles.css";
 
 function AvisoLegalComponents() {
   return (
     <>
-   <h1>Avisos Legales</h1>
-   <div className="div">   
+      <h1>Avisos Legales</h1>
+
+      <div className="div">
         En cumplimiento del artículo 10 de la Ley 34/2002, de 11 de julio, 
         de Servicios de la Sociedad de la Información y Comercio Electrónico te informamos que el Titular del sitio web superadmin.
         es (en adelante, el 'sitio Web'), es Superadmin S.L. (en adelante, 'Superadmin'), 
         con NIF B-26563197 y domicilio social en calle Lardero N7, 5H - 26002 Logroño e inscrita en el Registro Mercantil de La Rioja en el Tomo 851, 
         libro 0, folio 128, hoja LO-18470, inscripción primera.
-        Puedes contactar con Superadmin a través del correo
-        electrónico ayuda@superadmin.es y a través de las 
-        formas de contacto que encontrarás en <a href=" https://superadmin.es/contacto"> https://superadmin.es/contacto</a>
-   </div>
-   <h2 className="h2">1. Objeto</h2>
-   <div className="div2">
+        Puedes contactar con Superadmin a través del correo electrónico ayuda@superadmin.es y a través de las 
+        formas de contacto que encontrarás en <a href="https://superadmin.es/contacto"> https://superadmin.es/contacto</a>
+      </div>
+
+      <h2 className="h2">1. Objeto</h2>
+      <div className="div2">
         La finalidad del sitio Web de Superadmin tiene por objeto informar sobre Superadmin, las actividades que realiza y de los
         Servicios que presta, atender las solicitudes de compra y facilitar a los clientes la gestión de los Servicios contratados.
-   </div>
-   <h2 className="h2">2. Contenido</h2>
-   <div className="div2">
+      </div>
+
+      <h2 className="h2">2. Contenido</h2>
+      <div className="div2">
         A través del sitio Web, Superadmin te facilita el acceso y la utilización de diversos contenidos que Superadmin o sus colaboradores han publicado por medio de Internet.
         A tal efecto, te obligas y comprometes a NO utilizar cualquiera de los contenidos del sitio Web con fines o efectos ilícitos, prohibidos en este Aviso Legal o por la legislación vigente, lesivos de los derechos e intereses de terceros, o que de cualquier forma puedan dañar, inutilizar, sobrecargar, deteriorar o impedir la normal utilización de los contenidos, los equipos informáticos o los documentos, archivos y toda clase de contenidos almacenados en cualquier equipo informático propios o contratados por Superadmin, de otros Usuarios o de cualquier usuario de Internet.
         En todo caso, Superadmin se compromete a la retirada inmediata de cualquier contenido que pudiera contravenir la legislación nacional o internacional, la moral o el orden público, y ponerlo en conocimiento de las autoridades competentes.
-   </div>
-   <h2 className="h2">3. Medidas de seguridad</h2>
-   <div className="div2">
+      </div>
+
+      <h2 className="h2">3. Medidas de seguridad</h2>
+      <div className="div2">
         El sitio Web utiliza un certificado de seguridad TLS para asegurar la autenticidad del servidor y la integridad y cifrado de las comunicaciones entre el Usuario y Superadmin.
-        No obstante, debes ser consciente de que las medidas de seguridad de los sistemas informáticos en Internet no son enteramente fiables y que, por tanto Superadmin no puede garantizar
-         la inexistencia de virus u otros elementos que puedan producir alteraciones en los sistemas informáticos 
+        No obstante, debes ser consciente de que las medidas de seguridad de los sistemas informáticos en Internet no son enteramente fiables y que, por tanto Superadmin no puede garantizar la inexistencia de virus u otros elementos que puedan producir alteraciones en los sistemas informáticos 
         (software y hardware) del Usuario o en sus documentos electrónicos y ficheros contenidos en los mismos. Aunque Superadmin pone todos los medios necesarios y 
         toma las medidas de seguridad oportunas para evitar la presencia de estos elementos dañinos.
-   </div>
-   <h2 className="h2">4. Propiedad intelectual</h2>
-   <div className="div2">
+      </div>
+
+      <h2 className="h2">4. Propiedad intelectual</h2>
+      <div className="div2">
         El sitio web Superadmin, incluyendo a título enunciativo pero no limitativo su programación, diseño y demás elementos necesarios para su funcionamiento, logotipos, texto, gráficos e 
         imágenes son propiedad de Superadmin correspondiéndonos el ejercicio exclusivo de los derechos de explotación.
         Los diseños, logotipos, textos y gráficos ajenos a Superadmin y que pudieran aparecer en el sitio Web, pertenecen a sus respectivos propietarios. Superadmin reconoce a favor de sus titulares
         los correspondientes derechos de propiedad intelectual e industrial, no implicando su sola mención o aparición en el sitio Web la existencia de derechos o responsabilidad alguna sobre los mismos, 
         como tampoco respaldo, patrocinio o recomendación por parte de Superadmin.
-   </div>
-   <h2 className="h2">5. Legislación aplicable y Jurisdicción</h2>
-   <div className="div2">
+      </div>
+
+      <h2 className="h2">5. Legislación aplicable y Jurisdicción</h2>
+      <div className="div2">
         Para la resolución de todas las controversias o cuestiones relacionadas con el sitio Web o de las actividades en él desarrolladas, será de aplicación la legislación española, a la que se someten 
         expresamente las partes, siendo competentes para la resolución de todos los conflictos derivados o relacionados con su uso los Juzgados y Tribunales más cercanos a Logroño.
         En caso de que cualquier cláusula del presente documento sea declarada nula, las demás cláusulas seguirán vigentes y se interpretarán teniendo en cuenta la voluntad de las partes y la finalidad misma de las presentes condiciones.
-   </div>
-   <h2 className="h2">6. Exención de responsabilidades</h2>
-   <div className="div2">
+      </div>
+
+      <h2 className="h2">6. Exención de responsabilidades</h2>
+      <div className="div2">
         Superadmin no garantiza la inexistencia de errores en el acceso o en el contenido del sitio Web o del Blog, ni que estos se encuentren actualizados, aunque Superadmin realizará los mayores esfuerzos para evitarlos, corregirlos o para mantener actualizada su información.
         Sueradmin no se hace responsable por los daños que se puedan causar en los equipos o sistemas de los usuarios por posibles virus informáticos que se hubieran podido contraer por la navegación del usuario en el sitio Web.
-        
-   </div>
-   </>
-  )
+      </div>
+
+    </>
+  );
 }
 
-export default AvisoLegalComponents
+export default AvisoLegalComponents;
--- my-vue-app/src/components/Devlogcomponents.tsx
+++ my-vue-app/src/components/Devlogcomponents.tsx
@@ -1,87 +1,78 @@
-import "../styless/DevlogStyles.css"
+import "../styless/DevlogStyles.css";
 import AspectRatio from '@mui/joy/AspectRatio';
-
 
 function Devlogcomponents() {
   return (
-
-      <body className='body-Devlog'>   
- 
+    <body className='body-Devlog'>
+      
       <h1 className="desarrolladores-">
         !Desarrolladores!
       </h1>
-      
-        <h3 className="-somos">
+
+      <h3 className="-somos">
         Apartado de versiones del juego, Donde los usuarios 
         podran visualizar y acceder a las últimas correcciones 
         implementadas.
-        </h3>
+      </h3>
 
       <div className="BOX"> 
-
-      <AspectRatio className="CAJA" sx={{ width: 550 }}>
-        <p className="imagen-DEV"></p>
-      </AspectRatio> 
-      <h2 className="titulo-imagenn">Actualizacion 1.4</h2>
-      <p className="descr">
-      Hemos lanzado una nueva actualización en nuestra página
-      web, enfocada en mejorar la experiencia de usuario y optimizar 
-      las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
-      de un sistema de comentarios más eficiente en la sección de comunidad, 
-      con moderación automática para evitar spam y promover un ambiente seguro.
-      </p>
-
-        </div>
+        <AspectRatio className="CAJA" sx={{ width: 550 }}>
+          <p className="imagen-DEV"></p>
+        </AspectRatio>
+        <h2 className="titulo-imagenn">Actualizacion 1.4</h2>
+        <p className="descr">
+          Hemos lanzado una nueva actualización en nuestra página
+          web, enfocada en mejorar la experiencia de usuario y optimizar 
+          las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
+          de un sistema de comentarios más eficiente en la sección de comunidad, 
+          con moderación automática para evitar spam y promover un ambiente seguro.
+        </p>
+      </div>
 
       <div className="BOX"> 
-      <AspectRatio className="CAJA" sx={{ width: 550 }}>
-        <p className="imagen-DEV"></p>
-      </AspectRatio> 
-      <h2 className="titulo-imagenn">Actualizacion1.3</h2>
-      <p className="descr">
-      Hemos lanzado una nueva actualización en nuestra página
-      web, enfocada en mejorar la experiencia de usuario y optimizar 
-      las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
-      de un sistema de comentarios más eficiente en la sección de comunidad, 
-      con moderación automática para evitar spam y promover un ambiente seguro.
-      </p>
-        </div>
-          <div className="BOX"> 
+        <AspectRatio className="CAJA" sx={{ width: 550 }}>
+          <p className="imagen-DEV"></p>
+        </AspectRatio>
+        <h2 className="titulo-imagenn">Actualizacion 1.3</h2>
+        <p className="descr">
+          Hemos lanzado una nueva actualización en nuestra página
+          web, enfocada en mejorar la experiencia de usuario y optimizar 
+          las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
+          de un sistema de comentarios más eficiente en la sección de comunidad, 
+          con moderación automática para evitar spam y promover un ambiente seguro.
+        </p>
+      </div>
 
+      <div className="BOX"> 
+        <AspectRatio className="CAJA" sx={{ width: 550 }}>
+          <p className="imagen-DEV"></p>
+        </AspectRatio>
+        <h2 className="titulo-imagenn">Actualizacion 1.2</h2>
+        <p className="descr">
+          Hemos lanzado una nueva actualización en nuestra página
+          web, enfocada en mejorar la experiencia de usuario y optimizar 
+          las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
+          de un sistema de comentarios más eficiente en la sección de comunidad, 
+          con moderación automática para evitar spam y promover un ambiente seguro.
+        </p>
+      </div>
 
+      <div className="BOX"> 
+        <AspectRatio className="CAJA" sx={{ width: 550 }}>
+          <p className="imagen-DEV"></p>
+        </AspectRatio>
+        <h2 className="titulo-imagenn">Actualizacion 1.1</h2>
+        <p className="descr">
+          Hemos lanzado una nueva actualización en nuestra página
+          web, enfocada en mejorar la experiencia de usuario y optimizar 
+          las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
+          de un sistema de comentarios más eficiente en la sección de comunidad, 
+          con moderación automática para evitar spam y promover un ambiente seguro.
+        </p>
+      </div>
 
-
-      <AspectRatio className="CAJA" sx={{ width: 550 }}>
-        <p className="imagen-DEV"></p>
-      </AspectRatio> 
-      <h2 className="titulo-imagenn">Actualizacion 1.2</h2>
-      <p className="descr">
-      Hemos lanzado una nueva actualización en nuestra página
-      web, enfocada en mejorar la experiencia de usuario y optimizar 
-      las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
-      de un sistema de comentarios más eficiente en la sección de comunidad, 
-      con moderación automática para evitar spam y promover un ambiente seguro.
-      </p>
-
-        </div>
-         <div className="BOX"> 
-
-      <AspectRatio className="CAJA" sx={{ width: 550 }}>
-        <p className="imagen-DEV"></p>
-      </AspectRatio> 
-      <h2 className="titulo-imagenn">Actualizacion 1.1</h2>
-      <p className="descr">
-      Hemos lanzado una nueva actualización en nuestra página
-      web, enfocada en mejorar la experiencia de usuario y optimizar 
-      las funcionalidades del sitio. Ahora, los usuarios podrán disfrutar 
-      de un sistema de comentarios más eficiente en la sección de comunidad, 
-      con moderación automática para evitar spam y promover un ambiente seguro.
-      </p>
-
-        </div>
- 
     </body>
-
-  )
+  );
 }
+
 export default Devlogcomponents;
