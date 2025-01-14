8e66aeb5a34f915be85e89c83e083c6425938434
dawe-vk <nicolassanhhh@gmail.com>
2025-01-14 12:50:44 -0600

    ALOSOYYO
    ELQUIEN?

--- my-vue-app/src/pages/Comunidad.tsx
+++ my-vue-app/src/pages/Comunidad.tsx
@@ -1,10 +1,13 @@
 import Footercomponents from "../components/Footercomponents";
 import Comunidadcomponents from "../components/Comunidadcomponents";
+import "../Carpetacomponente2/RealHeader"
+import ComponenteDeComponenteHeader from "../Carpetacomponente2/RealHeader";
 
 
 function Comunidad() {
     return (
         <>
+            <ComponenteDeComponenteHeader />
             <Comunidadcomponents />
             <Footercomponents />
         </>
--- my-vue-app/src/pages/SobreEljuego.tsx
+++ my-vue-app/src/pages/SobreEljuego.tsx
@@ -1,11 +1,14 @@
 
 import Footercomponents from "../components/Footercomponents"
 import Sobrejuego from "../components/Sobrejuego"
+import "../Carpetacomponente2/RealHeader"
+import ComponenteDeComponenteHeader from "../Carpetacomponente2/RealHeader"
 
 function SobreEljuego() {
     return (
-      <>
-
+      <> 
+        <ComponenteDeComponenteHeader/>
+        
         <Sobrejuego />
         
         <Footercomponents /> 
