953c1ae0fb8b95a3e0420fbae588a12ed151650d
dawe-vk <nicolassanhhh@gmail.com>
2025-01-15 10:32:43 -0600

    latontera
    ayudame jesu

--- my-vue-app/src/pages/Comunidad.tsx
+++ my-vue-app/src/pages/Comunidad.tsx
@@ -2,7 +2,6 @@ import Footercomponents from "../components/Footercomponents";
 import Comunidadcomponents from "../components/Comunidadcomponents";
 import "../Carpetacomponente2/RealHeader"
 import ComponenteDeComponenteHeader from "../Carpetacomponente2/RealHeader";
-import { useEffect, useState } from "react";
 
 
 
