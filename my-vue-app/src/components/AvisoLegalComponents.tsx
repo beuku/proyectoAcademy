f329f9dee8a368a1d7482e0eac7f7d8e3aad4681
JostynBateojaOrtiz <jostynortiz87@gmail.com>
2025-01-23 10:20:46 -0300

    avisolegal

--- my-vue-app/src/components/AvisoLegalComponents.tsx
+++ my-vue-app/src/components/AvisoLegalComponents.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from "@mui/material";
 import "../styless/AvisoLegalStyles.css";
 import IndiceLateral from "./IndiceLateralComponents";
 
@@ -5,20 +6,19 @@ import IndiceLateral from "./IndiceLateralComponents";
 function AvisoLegalComponents() {
   return (
     <>
+    <Typography variant="h1" >
+        Avisos Legales
+    </Typography>
     
-    <h1>Avisos Legales</h1>
-
-    <div className="div">
-      En cumplimiento del artículo 10 de la Ley 34/2002, de 11 de julio, 
+    <Box className="div">En cumplimiento del artículo 10 de la Ley 34/2002, de 11 de julio, 
       de Servicios de la Sociedad de la Información y Comercio Electrónico te informamos que el Titular del sitio web superadmin.
       es (en adelante, el 'sitio Web'), es Superadmin S.L. (en adelante, 'Superadmin'), 
       con NIF B-26563197 y domicilio social en calle Lardero N7, 5H - 26002 Logroño e inscrita en el Registro Mercantil de La Rioja en el Tomo 851, 
       libro 0, folio 128, hoja LO-18470, inscripción primera.
       Puedes contactar con Superadmin a través del correo electrónico ayuda@superadmin.es y a través de las 
       formas de contacto que encontrarás en <a href="https://superadmin.es/contacto"> https://superadmin.es/contacto</a>
-    </div>
-
-      <IndiceLateral />
+    </Box>
+    <IndiceLateral />
 
     </>
   );
